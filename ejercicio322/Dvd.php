b89fc483875efc165e6f299e1c9faee0a9ef9ae9
Chris <mendozachristopher833@gmail.com>
2023-11-14 20:50:17 +0100

    añadido ejercicio 322

---
+++ ejercicio322/Dvd.php
@@ -0,0 +1,23 @@
+<?php
+include "./../ejercicio320/Soporte.php";
+class Dvd extends Soporte {
+    public string $idiomas;
+    private string $formatPantalla;
+
+    public function __construct(string $titulo, int $numero, float $precio, string $idiomas, string $formatPantalla) {
+        parent::__construct($titulo, $numero, $precio);
+        $this->idiomas = $idiomas;
+        $this->formatPantalla = $formatPantalla;
+    }
+
+    public function muestraResumen(): void {
+        $precio = parent::getPrecio();
+        echo "
+            <span>Película en DVD:</span><br>
+            <span>$this->titulo</span><br>
+            <span>$precio € (IVA no incluido)</span><br>
+            <span>Idiomas: $this->idiomas</span><br>
+            <span>Formato Pantalla: $this->formatPantalla</span>
+        ";
+    }
+}
