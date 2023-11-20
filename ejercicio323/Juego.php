fdea40cb1e0df9c96688d13f2a85ef41bb92679e
Chris <mendozachristopher833@gmail.com>
2023-11-20 17:44:46 +0100

    añadido ejercicio 325

--- ejercicio323/Juego.php
+++ ejercicio323/Juego.php
@@ -1,1 +1,39 @@
 <?php
+include "./../ejercicio320/Soporte.php";
+class Juego extends Soporte{
+    public string $consola;
+    private int $minNumJugadores;
+    private int $maxNumJugadores;
+
+    public function __construct(string $titulo, int $numero, float $precio, string $consola, int $minNumJugadores, int $maxNumJugadores) {
+        parent::__construct($titulo, $numero, $precio);
+        $this->consola = $consola;
+        $this->minNumJugadores = $minNumJugadores;
+        $this->maxNumJugadores = $maxNumJugadores;
+    }
+
+    public function muestraJugadoresPosibles(){
+        if($this->minNumJugadores ==1 and $this->maxNumJugadores ==1){
+            return "Para un jugador";
+        }
+        if($this->minNumJugadores == 1 and $this->maxNumJugadores>1){
+            return "De $this->minNumJugadores a $this->maxNumJugadores jugadores";
+        }
+        if($this->minNumJugadores==$this->maxNumJugadores){
+            return "Para $this->minNumJugadores jugadores";
+        }
+    }
+
+    public function muestraResumen(): void {
+        $precio = parent::getPrecio();
+        $jugadores = $this->muestraJugadoresPosibles();
+        echo "
+            <span>Juego para: $this->consola</span><br>
+            <span>Juego para $this->titulo</span><br>
+            <span>$precio € (IVA no incluido)</span><br>
+            <span>$jugadores</span><br>
+        ";
+    }
+
+
+}
--- ejercicio324/Cliente.php
+++ ejercicio324/Cliente.php
@@ -1,1 +1,57 @@
 <?php
+class Cliente{
+    public string $nombre;
+    private int $numero;
+    private $soportesAlquilados = array();
+    private int $numSoportesAlquilados;
+    private int $maxAlquilerConcurrente;
+
+    public function __construct(string $nombre, int $numero, int $maxAlquilerConcurrente=3) {
+        $this->nombre = $nombre;
+        $this->numero = numero;
+        $this->maxAlquilerConcurrente = $maxAlquilerConcurrente;
+    }
+
+    public function getNumero(){
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero){
+        $this->numero = $numero;
+    }
+
+    public function getNumSoportesAlquilados(){
+        return $this->numSoportesAlquilados;
+    }
+
+    public function muestraResumen(){
+        $cantidad = count($this->soportesAlquilados);
+        echo "
+        <span>$this->nombre</span><br>
+        <span>Cantidad de alquileres: $cantidad</span><br>
+        ";
+    }
+
+    public function tieneAlquilado(Soporte $s):bool{
+        foreach ($this->soportesAlquilados as $soporte){
+            if($soporte==$s){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function alquilar(Soporte $s):bool{
+        if($this->tieneAlquilado($s) and $this->numSoportesAlquilados<$this->maxAlquilerConcurrente){
+            //si está alquilado y no supera el numero máximo, realizamos la operacion.
+            $this->numSoportesAlquilados++;
+            array_push($this->soportesAlquilados, $s);
+            echo "<span>Se ha alquilado.</span><br>";
+            return true;
+        }
+        //en caso contrario, no realizamos nada, y devolvemos false;
+        echo "<span>No se ha podido alquilar.</span><br>";
+        return false;
+
+    }
+}
